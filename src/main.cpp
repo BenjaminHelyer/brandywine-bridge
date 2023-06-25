82593c1643cbb911104f52800fb7f115e3b974bf
BenjaminHelyer <91896754+BenjaminHelyer@users.noreply.github.com>
2023-06-25 08:54:54 -0500

    Added routes for read and create.

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,22 @@ int main(void)
 
     // set the first get request, passing our response function as a callback
     svr.Get("/hi", [&svrApiLayer](const httplib::Request& req, httplib::Response& res) {
-        std::string resp = svrApiLayer.hello_world_response();
-        res.set_content(resp, "text/plain");
+        std::string hello = svrApiLayer.hello_world_response();
+        res.set_content(hello, "text/plain");
+    });
+
+    // pattern match two strings on this create route
+    svr.Get(R"(/create/([^/]+)/([^/]+))", [&svrApiLayer](const httplib::Request& req, httplib::Response& res) {
+        auto key = req.matches[1];
+        auto val = req.matches[2];
+        std::string confirmation = svrApiLayer.create_key(key, val);
+        res.set_content(confirmation, "text/plain");
+    });
+
+    svr.Get(R"(/read/([^/]+))", [&svrApiLayer](const httplib::Request& req, httplib::Response& res) {
+        auto key = req.matches[1];
+        std::string confirmation = svrApiLayer.read_key(key);
+        res.set_content(confirmation, "text/plain");
     });
 
     svr.listen("localhost", 8080);
