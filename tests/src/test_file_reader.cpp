7390f4a08ff51d6bdb863a47f9f7b5f7dd183f18
BenjaminHelyer <91896754+BenjaminHelyer@users.noreply.github.com>
2023-07-07 09:58:26 -0500

    Filled out all test descriptions for FileReader class.

--- tests/src/test_file_reader.cpp
+++ tests/src/test_file_reader.cpp
@@ -6,7 +6,53 @@ protected:
     FileReaderTest() = default;
 };
 
-TEST_F(FileReaderTest, placeholderCheck)
+/*
+* Test with a basic write operation on the file.
+* Confirms that no errors are raised during this operation.
+*/
+TEST_F(FileReaderTest, writeIsValid)
 {
+    EXPECT_EQ("", "");
+}
+
+/*
+* Test with a basic read operation on a known file.
+* Confirms that no errors are raised during the operation.
+*/
+TEST_F(FileReaderTest, readIsValid)
+{
+    EXPECT_EQ("", "");
+}
+
+/*
+* Test with a write followed by a read.
+* Ensures that the operations perform as expected.
+*/
+TEST_F(FileReaderTest, writeThenReadIsSound) {
+    EXPECT_EQ("", "");
+}
+
+/*
+* Test with a read operation given a byte offset
+* that is past the end of the file.
+* Checks to see if we raise the appropriate error.
+*/
+TEST_F(FileReaderTest, badOffsetRaisesError) {
+    EXPECT_EQ("", "");
+}
+
+/*
+* Test to read on a deleted file, ensuring that we
+* raise the appropriate error.
+*/
+TEST_F(FileReaderTest, readDeletedFileRaisesError) {
+    EXPECT_EQ("", "");
+}
+
+/*
+* Test to write on a deleted file, ensuring that we
+* raise the appropriate error.
+*/
+TEST_F(FileReaderTest, writeDeletedFileRaisesError) {
     EXPECT_EQ("", "");
 }
